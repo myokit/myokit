6f5723845f1e12d90068611da36b44ada80f4b59
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-02-28 20:05:05 +0000

    Testing travis-ci

---
+++ test/travis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# Runs the unit tests / developer tests for myokit
+#
+# This file is part of Myokit
+#  Copyright 2011-2018 Maastricht University, University of Oxford
+#  Licensed under the GNU General Public License v3.0
+#  See: http://myokit.org
+#
+import sys
+import myotest
+result = myotest.run_all()
+sys.exit(0 if result else 1)
