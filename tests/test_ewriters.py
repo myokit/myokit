3d3a6b6f7dc63f191292dd00f047ec40c9545410
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-04-08 14:33:25 +0100

    Added latex ewriter test.

--- tests/test_ewriters.py
+++ tests/test_ewriters.py
@@ -13,6 +13,7 @@ from __future__ import print_function, unicode_literals
 import unittest
 import myokit
 import myokit.formats
+import myokit.formats.latex
 import myokit.formats.python
 
 
@@ -61,7 +62,7 @@ import myokit.formats.python
 
 class PythonExpressionWriterTest(unittest.TestCase):
 
-    def test_name_number(self):
+    def test_all(self):
         w = myokit.formats.python.PythonExpressionWriter()
 
         model = myokit.Model()
@@ -191,7 +192,7 @@ class PythonExpressionWriterTest(unittest.TestCase):
 
 class NumpyExpressionWriterTest(unittest.TestCase):
 
-    def test_name_number(self):
+    def test_all(self):
         w = myokit.formats.python.NumpyExpressionWriter()
 
         model = myokit.Model()
@@ -318,6 +319,150 @@ class NumpyExpressionWriterTest(unittest.TestCase):
         # Test fetching using ewriter method
         w = myokit.formats.ewriter('numpy')
         self.assertIsInstance(w, myokit.formats.python.NumpyExpressionWriter)
+
+
+class LatexExpressionWriterTest(unittest.TestCase):
+
+    def test_all(self):
+        w = myokit.formats.latex.LatexExpressionWriter()
+
+        model = myokit.Model()
+        component = model.add_component('c')
+        avar = component.add_variable('a')
+
+        # Model needs to be validated --> sets unames
+        avar.set_rhs(12)
+        avar.set_binding('time')
+        model.validate()
+
+        # Name
+        a = myokit.Name(avar)
+        self.assertEqual(w.ex(a), '\\text{a}')
+        # Number
+        b = myokit.Number(12)
+        self.assertEqual(w.ex(b), '12.0')
+
+        # Plus
+        x = myokit.Plus(a, b)
+        self.assertEqual(w.ex(x), '\\text{a}+12.0')
+        # Minus
+        x = myokit.Minus(a, b)
+        self.assertEqual(w.ex(x), '\\text{a}-12.0')
+        # Multiply
+        x = myokit.Multiply(a, b)
+        self.assertEqual(w.ex(x), '\\text{a}*12.0')
+        # Divide
+        x = myokit.Divide(a, b)
+        self.assertEqual(w.ex(x), '\\frac{\\text{a}}{12.0}')
+
+        # Quotient
+        # Not supported in latex!
+        x = myokit.Quotient(a, b)
+        self.assertEqual(w.ex(x), 'quotient\\left(\\text{a},12.0\\right)')
+        # Remainder
+        x = myokit.Remainder(a, b)
+        self.assertEqual(w.ex(x), '\\bmod\\left(\\text{a},12.0\\right)')
+
+        # Power
+        x = myokit.Power(a, b)
+        self.assertEqual(w.ex(x), '\\text{a}^{12.0}')
+        # Sqrt
+        x = myokit.Sqrt(b)
+        self.assertEqual(w.ex(x), '\\sqrt{12.0}')
+        # Exp
+        x = myokit.Exp(a)
+        self.assertEqual(w.ex(x), '\\exp\\left(\\text{a}\\right)')
+        # Log(a)
+        x = myokit.Log(b)
+        self.assertEqual(w.ex(x), '\\log\\left(12.0\\right)')
+        # Log(a, b)
+        x = myokit.Log(a, b)
+        self.assertEqual(w.ex(x), '\\log_{12.0}\\left(\\text{a}\\right)')
+        # Log10
+        x = myokit.Log10(b)
+        self.assertEqual(w.ex(x), '\\log_{10.0}\\left(12.0\\right)')
+
+        # Sin
+        x = myokit.Sin(b)
+        self.assertEqual(w.ex(x), '\\sin\\left(12.0\\right)')
+        # Cos
+        x = myokit.Cos(b)
+        self.assertEqual(w.ex(x), '\\cos\\left(12.0\\right)')
+        # Tan
+        x = myokit.Tan(b)
+        self.assertEqual(w.ex(x), '\\tan\\left(12.0\\right)')
+        # ASin
+        x = myokit.ASin(b)
+        self.assertEqual(w.ex(x), '\\arcsin\\left(12.0\\right)')
+        # ACos
+        x = myokit.ACos(b)
+        self.assertEqual(w.ex(x), '\\arccos\\left(12.0\\right)')
+        # ATan
+        x = myokit.ATan(b)
+        self.assertEqual(w.ex(x), '\\arctan\\left(12.0\\right)')
+
+        # Floor
+        x = myokit.Floor(b)
+        self.assertEqual(w.ex(x), '\\left\\lfloor{12.0}\\right\\rfloor')
+        # Ceil
+        x = myokit.Ceil(b)
+        self.assertEqual(w.ex(x), '\\left\\lceil{12.0}\\right\\rceil')
+        # Abs
+        x = myokit.Abs(b)
+        self.assertEqual(w.ex(x), '\\lvert{12.0}\\rvert')
+
+        # Equal
+        x = myokit.Equal(a, b)
+        self.assertEqual(w.ex(x), '\\left(\\text{a}=12.0\\right)')
+        # NotEqual
+        x = myokit.NotEqual(a, b)
+        self.assertEqual(w.ex(x), '\\left(\\text{a}\\neq12.0\\right)')
+        # More
+        x = myokit.More(a, b)
+        self.assertEqual(w.ex(x), '\\left(\\text{a}>12.0\\right)')
+        # Less
+        x = myokit.Less(a, b)
+        self.assertEqual(w.ex(x), '\\left(\\text{a}<12.0\\right)')
+        # MoreEqual
+        x = myokit.MoreEqual(a, b)
+        self.assertEqual(w.ex(x), '\\left(\\text{a}\\geq12.0\\right)')
+        # LessEqual
+        x = myokit.LessEqual(a, b)
+        self.assertEqual(w.ex(x), '\\left(\\text{a}\\leq12.0\\right)')
+
+        # Not
+        cond1 = myokit.parse_expression('5 > 3')
+        cond2 = myokit.parse_expression('2 < 1')
+        x = myokit.Not(cond1)
+        self.assertEqual(
+            w.ex(x), '\\not\\left(\\left(5.0>3.0\\right)\\right)')
+        # And
+        x = myokit.And(cond1, cond2)
+        self.assertEqual(
+            w.ex(x),
+            '\\left(\\left(5.0>3.0\\right)\\and'
+            '\\left(2.0<1.0\\right)\\right)')
+        # Or
+        x = myokit.Or(cond1, cond2)
+        self.assertEqual(
+            w.ex(x),
+            '\\left(\\left(5.0>3.0\\right)\\or'
+            '\\left(2.0<1.0\\right)\\right)')
+        # If
+        x = myokit.If(cond1, a, b)
+        self.assertEqual(
+            w.ex(x), 'if\\left(\\left(5.0>3.0\\right),\\text{a},12.0\\right)')
+        # Piecewise
+        c = myokit.Number(1)
+        x = myokit.Piecewise(cond1, a, cond2, b, c)
+        self.assertEqual(
+            w.ex(x),
+            'piecewise\\left(\\left(5.0>3.0\\right),\\text{a},'
+            '\\left(2.0<1.0\\right),12.0,1.0\\right)')
+
+        # Test fetching using ewriter method
+        w = myokit.formats.ewriter('latex')
+        self.assertIsInstance(w, myokit.formats.latex.LatexExpressionWriter)
 
 
 if __name__ == '__main__':
