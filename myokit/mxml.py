5d63576d18077303e51450e15864e03da3206c0a
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-04-11 20:31:12 +0100

    Added test for myokit.mxml html2ascii method.

--- myokit/mxml.py
+++ myokit/mxml.py
@@ -65,9 +65,10 @@ def dom_next(node, selector=False):
 
 def html2ascii(html, width=79, indent='  '):
     """
-    Flattens HTML and attempts to create readable ASCII code.
+    Flattens HTML and attempts to create readable ASCII code (actually utf-8
+    rather than ascii).
 
-    The ascii will be text-wrapped after ``width`` characters. Each new level
+    The output will be text-wrapped after ``width`` characters. Each new level
     of nesting will be indented with the text given as ``indent``.
     """
     class Asciifier(HTMLParser.HTMLParser):
@@ -76,11 +77,23 @@ def html2ascii(html, width=79, indent='  '):
         WHITE = [' ', '\t', '\f', '\r', '\n']
 
         def __init__(self, line_width=79, indent='  '):
-            HTMLParser.HTMLParser.__init__(self)  # HTMLParser is old school
+            # Create HTMLParser with old-school constructor
+            # Python 2:
+            HTMLParser.HTMLParser.__init__(self)
+            # Python 3:
+            #TODO Add argument convert_charrefs=False
+
+            # In <head> yes/no
+            self.inhead = False
+
             self.text = []  # Current document
             self.line = []  # Current (unwrapped) line
-            self.limode = None
-            self.licount = 0
+
+            # Ordered/unorderd lists
+            self.limode = []
+            self.licount = []
+
+            # Output options
             self.LW = line_width
             self.TAB = indent
 
@@ -109,76 +122,128 @@ def html2ascii(html, width=79, indent='  '):
             """
             Handle text between tags
             """
-            data = str(data.strip().encode('ascii', 'ignore'))
+            # Ignore content while in head
+            if self.inhead:
+                return
+
+            # Decode html characters & add to line
+            data = str(data.encode('utf8', 'ignore'))
             if data:
                 self.line.append(data)
 
         def handle_starttag(self, tag, attrs):
-            """
-            Opening tags
-            """
-            if tag == 'p':
+            """ Called when a start tag is encountered. """
+            if tag == 'head':
+                self.inhead = True
+
+            elif tag == 'p':
+                self.endline()
                 self.blankline()
+
             elif tag == 'h1':
+                self.endline()
+                self.blankline()
                 self.text.append('=' * self.LW)
+
             elif tag == 'h2':
+                self.endline()
                 self.blankline()
                 self.text.append('-' * self.LW)
+
             elif tag == 'h3':
+                self.endline()
                 self.blankline()
                 self.text.append('.' * self.LW)
+
             elif tag == 'ul' or tag == 'ol':
                 self.endline()
+                self.blankline()
                 self.text.append(self.INDENT)
-                self.limode = tag
-                self.licount = 0
+                self.limode.append(tag)
+                self.licount.append(0)
+
             elif tag == 'li':
-                if self.limode == 'ul':
+                self.endline()
+                # Get mode and count, or use default if not set
+                if len(self.limode):
+                    limode = self.limode[-1]
+                    licount = self.licount[-1]
+                    self.licount[-1] += 1
+                else:
+                    limode = 'ul'
+                    licount = 0
+
+                # Add bullet
+                if limode == 'ul':
                     self.line.append('* ')
                 else:
-                    self.licount += 1
-                    self.line.append(str(self.licount) + ' ')
+                    self.line.append(str(1 + licount) + ' ')
+
             elif tag == 'em' or tag == 'i':
                 self.line.append(' *')
+
             elif tag == 'strong' or tag == 'b':
                 self.line.append(' **')
+
             elif tag == 'u':
                 self.line.append(' _')
 
         def handle_startendtag(self, tag, attrs):
+            """ Called when an open/end tag is encountered. """
             if tag == 'br':
                 self.endline()
             elif tag == 'hr':
                 self.text.append('-' * self.LW)
 
         def handle_endtag(self, tag):
-            if tag == 'p':
+            """ Called when an endtag is encountered. """
+
+            if tag == 'head':
+                self.inhead = False
+
+            elif tag == 'p':
                 self.endline()
                 self.blankline()
+
             elif tag == 'h1':
                 self.endline()
                 self.text.append('=' * self.LW)
                 self.blankline()
+
             elif tag == 'h2':
                 self.endline()
                 self.text.append('-' * self.LW)
                 self.blankline()
+
             elif tag == 'h3':
                 self.endline()
                 self.text.append('.' * self.LW)
                 self.blankline()
+
             elif tag == 'ul' or tag == 'ol':
                 self.endline()
                 self.text.append(self.DEDENT)
                 self.blankline()
+
+                if len(self.limode):
+                    self.limode.pop()
+                if len(self.licount):
+                    self.licount.pop()
+
             elif tag == 'li':
-                self.endline()
+                pass
+
             elif tag == 'em' or tag == 'i':
                 self.line.append('* ')
+
             elif tag == 'strong' or tag == 'b':
                 self.line.append('** ')
+
             elif tag == 'u':
                 self.line.append('_ ')
+
+        def handle_entityref(self, name):
+            self.line.append(self.unescape('&' + name + ';'))
 
         def gettext(self):
             self.endline()
@@ -189,6 +254,7 @@ def html2ascii(html, width=79, indent='  '):
             dent = 0
             white = '[' + ''.join(self.WHITE) + ']+'
             space = ' '
+            next_can_be_blank = False
             for line in self.text:
                 if line == self.INDENT:
                     dent += 1
@@ -201,10 +267,15 @@ def html2ascii(html, width=79, indent='  '):
                 else:
                     line = re.sub(white, space, line).strip()
                     if line == '':
-                        buf.append('')
+                        if next_can_be_blank:
+                            buf.append('')
+                            next_can_be_blank = False
                     else:
                         buf.extend([pre + x for x in textwrap.wrap(line, n)])
+                        next_can_be_blank = True
             return ('\n'.join(buf)).strip()
+
+    # Create asciifier and go!
     f = Asciifier(width, indent)
     f.feed(html)
     f.close()
