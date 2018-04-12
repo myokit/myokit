06c3ad73b8fa8b05a9b4c8bed074738c13ab1451
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-04-12 20:28:38 +0100

    Added test for saving without a file.

--- tests/test_loadsave.py
+++ tests/test_loadsave.py
@@ -260,6 +260,14 @@ class LoadSaveTest(unittest.TestCase):
             self.assertEqual(pp.code(), p.code())
             self.assertEqual(xx, None)
 
+        # Save all, compare with string generated version
+        with TemporaryDirectory() as d:
+            opath = d.path('test.mmt')
+            myokit.save(opath, model=m, protocol=p, script=x)
+            with open(opath, 'r') as f:
+                text = f.read()
+            self.assertEqual(text, myokit.save(model=m, protocol=p, script=x))
+
     def test_load_partial(self):
         """ Test loading of partial files. """
         mpath = os.path.join(DIR_DATA, 'beeler-1977-model.mmt')
