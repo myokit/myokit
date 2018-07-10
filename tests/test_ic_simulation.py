225d87bbfaba1c3a7bba4f559535fd4a42546a6a
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-07-10 16:52:16 +0100

    Added more tests for ICSimulation.

--- tests/test_ic_simulation.py
+++ tests/test_ic_simulation.py
@@ -15,7 +15,7 @@ import unittest
 
 import myokit
 
-from shared import DIR_DATA
+from shared import DIR_DATA, CancellingReporter
 
 
 class ICSimulationTest(unittest.TestCase):
@@ -30,7 +30,14 @@ class ICSimulationTest(unittest.TestCase):
 
         # Run a simulation
         s = myokit.ICSimulation(m, p)
+
+        self.assertEqual(s.time(), 0)
+        self.assertEqual(s.state(), m.state())
+        self.assertEqual(s.default_state(), m.state())
         d, e = s.run(20, log_interval=5)
+        self.assertEqual(s.time(), 20)
+        self.assertNotEqual(s.state(), m.state())
+        self.assertEqual(s.default_state(), m.state())
 
         # Create a datablock from the simulation log
         b = s.block(d, e)
@@ -48,6 +55,36 @@ class ICSimulationTest(unittest.TestCase):
 
         # Time can't be negative
         self.assertRaises(ValueError, s.run, -1)
+
+        # Test running without a protocol
+        s.set_protocol(None)
+        s.run(1)
+
+        # Test step size is > 0
+        self.assertRaises(ValueError, s.set_step_size, 0)
+
+        # Test negative log interval is ignored
+        s.run(1, log_interval=-1)
+
+    def test_progress_reporter(self):
+        """ Test running with a progress reporter. """
+        m, p, x = myokit.load(os.path.join(DIR_DATA, 'lr-1991.mmt'))
+
+        # Test using a progress reporter
+        s = myokit.ICSimulation(m, p)
+        with myokit.PyCapture() as c:
+            s.run(110, progress=myokit.ProgressPrinter())
+        c = c.text().splitlines()
+        self.assertTrue(len(c) > 0)
+
+        # Not a progress reporter
+        self.assertRaisesRegexp(
+            ValueError, 'ProgressReporter', s.run, 5, progress=12)
+
+        # Cancel from reporter
+        self.assertRaises(
+            myokit.SimulationCancelledError, s.run, 1,
+            progress=CancellingReporter(0))
 
     def test_invalid_model(self):
         """ Tests running with an invalid model. """
