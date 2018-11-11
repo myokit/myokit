0010725a391d5fe58beab7269a4717490ff0347a
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-11-11 23:07:21 +0000

    Updated appveyor opencl package versions.

--- .appveyor.ps1
+++ .appveyor.ps1
@@ -5,13 +5,14 @@
 #
 
 # Download Intel OpenCL SDK (unless already present), and install
-If (-not (Test-Path 'intel_sdk_for_opencl_setup_6.0.0.1049.exe')) {
-	Invoke-WebRequest http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/8539/intel_sdk_for_opencl_setup_6.0.0.1049.exe -OutFile intel_sdk_for_opencl_setup_6.0.0.1049.exe
+If (-not (Test-Path 'intel_sdk.exe')) {
+	Invoke-WebRequest http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/12527/intel_sdk_for_opencl_2017_7.0.0.2567.exe -OutFile intel_sdk.exe
 }
-.\intel_sdk_for_opencl_setup_6.0.0.1049.exe install --output=output.log --eula=accept | Out-Null
+.\intel_sdk.exe install --output=output.log --eula=accept | Out-Null
 
 # Download Intel OpenCL runtime (unless already present)
-If (-not (Test-Path 'opencl_runtime_16.1.1_x64_setup.msi')) {
-	Invoke-WebRequest http://registrationcenter-download.intel.com/akdlm/irc_nas/9022/opencl_runtime_16.1.1_x64_setup.msi -OutFile opencl_runtime_16.1.1_x64_setup.msi
+If (-not (Test-Path 'opencl_runtime.msi')) {
+	#Invoke-WebRequest http://registrationcenter-download.intel.com/akdlm/irc_nas/9022/opencl_runtime_16.1.1_x64_setup.msi -OutFile opencl_runtime.msi
+	Invoke-WebRequest http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13794/opencl_runtime_18.1_x64_setup.msi -OutFile opencl_runtime.msi
 }
-msiexec.exe /i opencl_runtime_16.1.1_x64_setup.msi /qn | Out-Null
+msiexec.exe /i opencl_runtime.msi /qn | Out-Null
