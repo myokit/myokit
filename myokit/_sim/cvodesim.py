d4fb342e916a510ef4979f390b7dd1df208494ad
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-01-24 00:13:30 +0000

    Fixed timing of pacing system update in cvode sim with dynamic logging.

--- myokit/_sim/cvodesim.py
+++ myokit/_sim/cvodesim.py
@@ -102,7 +102,7 @@ class Simulation(myokit.CModule):
             self._apd_var = self._model.get(apd_var)
             if not self._apd_var.is_state():
                 raise ValueError(
-                    'The potential variable must be a state variable.')
+                    'The `apd_var` must be a state variable.')
 
         # Get state and default state from model
         self._state = self._model.state()
