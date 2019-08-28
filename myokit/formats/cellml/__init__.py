ec76081e76f79631bd91dcc1c8b454ca56bd7e89
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-08-28 15:23:07 +0100

    Moved is_valid_identifier(name) into cellml module.

--- myokit/formats/cellml/__init__.py
+++ myokit/formats/cellml/__init__.py
@@ -13,6 +13,8 @@ from ._importer import CellMLImporter, CellMLError  # noqa
 from ._exporter import CellMLExporter
 from ._ewriter import CellMLExpressionWriter
 
+import re
+
 
 # Importers
 _importers = {
@@ -52,4 +54,23 @@ def ewriters():
     """
     return dict(_ewriters)
 
-# Language keywords
+
+# Valid CellML identifiers
+# Official docs allow silly things, e.g. 1e2 or 123 or _123
+# re.compile('^_*[a-zA-Z0-9][a-zA-Z0-9_]*$')
+# So let's be more strict:
+#  - At least one letter
+#  - Can't start with a number
+_cellml_identifier = re.compile('^([_][0-9_]*)?[a-zA-Z][a-zA-Z0-9_]*$')
+# Future versions will have:
+#   - Must start with at least one letter
+
+def is_valid_identifier(name):
+    """
+    Tests if the given ``name`` is a valid CellML 1 identifier.
+    """
+    return _cellml_identifier.match(name) is not None
+
+
+# Don't expose imported modules
+del(re)
