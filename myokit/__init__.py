0d3f61c65eb4a505b2dd38d92fe5ceffdbf333cf
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-10-11 14:35:48 +0100

    Updating license info.

--- myokit/__init__.py
+++ myokit/__init__.py
@@ -120,59 +120,89 @@ del(log, logging)
 
 # Full license text
 LICENSE = """
-Myokit
-Copyright 2011-2018 Maastricht University, University of Oxford
-michael@myokit.org
+Copyright (c) 2011-2017 Maastricht University. All rights reserved.
+Copyright (c) 2017-2019 University of Oxford. All rights reserved.
+ (University of Oxford means the Chancellor, Masters and Scholars of the
+  University of Oxford, having an administrative office at Wellington Square,
+  Oxford OX1 2JD, UK).
 
-Myokit is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your
-option) any later version.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-Myokit is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details.
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
 
-For a copy of the GNU General Public License,
-see http://www.gnu.org/licenses/.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """.strip()
 
 # Full license text, html
 LICENSE_HTML = """
 <h1>Myokit</h1>
 <p>
-    Copyright 2011-2018 Maastricht University, University of Oxford
-    <br /><a href="mailto:michael@myokit.org">michael@myokit.org</a>
+          Copyright (c) 2011-2017 Maastricht University. All rights reserved.
+    <br />Copyright (c) 2017-2019 University of Oxford. All rights reserved.
+    <br />(University of Oxford means the Chancellor, Masters and Scholars of
+    the University of Oxford, having an administrative office at Wellington
+    Square, Oxford OX1 2JD, UK).
 </p>
 <p>
-    Myokit is free software: you can redistribute it and/or modify it under the
-    terms of the GNU General Public License as published by the Free Software
-    Foundation, either version 3 of the License, or (at your option) any later
-    version.
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
 </p>
+<ul>
+    <li>
+        <p>
+            Redistributions of source code must retain the above copyright
+            notice, this list of conditions and the following disclaimer.
+        </p>
+    </li>
+    <li>
+        <p>
+            Redistributions in binary form must reproduce the above copyright
+            notice, this list of conditions and the following disclaimer in the
+            documentation and/or other materials provided with the
+            distribution.
+        </p>
+    </li>
+    <li>
+        <p>
+            Neither the name of the copyright holder nor the names of its
+            contributors may be used to endorse or promote products derived
+            from this software without specific prior written permission.
+        </p>
+    </li>
+</ul>
 <p>
-    Myokit is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-    details.
-</p>
-<p>
-    For a copy of the GNU General Public License, see
-    <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
 </p>
 """.strip()
-
-# Example license header that should appear in most files
-LICENSE_HEADER = """
-This file is part of Myokit
- Copyright 2011-2018 Maastricht University, University of Oxford
- Licensed under the GNU General Public License v3.0
- See: http://myokit.org
-""".strip()
-
-# Single-line copyright notice
-COPYRIGHT = '(C) 2011-2018, Maastricht University, University of Oxford'
 
 
 #
