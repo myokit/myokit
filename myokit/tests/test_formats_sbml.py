9aaca81ee826f654aeb9ce5359646327a0a056d2
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-05-31 16:21:15 +0100

    Added test for older SBML version.

--- myokit/tests/test_formats_sbml.py
+++ myokit/tests/test_formats_sbml.py
@@ -680,9 +680,10 @@ class SBMLParserTest(unittest.TestCase):
         self.assertBad(xml=xml, message='Unable to create Name:')
 
 
-class SBMLParserReactionsTest(unittest.TestCase):
+class SBMLDocumentTest(unittest.TestCase):
     """
-    Tests parsing an SBML file with species, compartments, and reactions.
+    Tests parsing an SBML file with species, compartments, and reactions, as
+    well as assignment rules.
 
     This tests uses a modified model (case 00004) from the SBML test suite
     http://sbml.org/Facilities/Database/.
@@ -866,6 +867,19 @@ class SBMLParserReactionsTest(unittest.TestCase):
         name = 'case00004'
         self.assertEqual(self.model.name(), name)
 
+    def test_older_version(self):
+        # Test loading the same model in an older SBML format.
+
+        # Parse model in older format
+        old_model = myokit.formats.sbml.SBMLParser().parse_file(os.path.join(
+            DIR_FORMATS, 'sbml', '00004-sbml-l2v1-modified.xml'))
+
+        # Set time units (Only introduced in level 3 version 1)
+        old_model.time().set_unit(self.model.time().unit())
+
+        # Now models should be equal
+        self.assertEqual(self.model.code(), old_model.code())
+
     def test_rate_expressions(self):
         # Tests whether state variables have been assigned with the correct
         # rate expression. Those may come from a rateRule or reaction.
@@ -1003,166 +1017,6 @@ class SBMLParserReactionsTest(unittest.TestCase):
         unit = None
         self.assertEqual(parameter.unit(), unit)
 
-
-'''
-class SBMLParserEquationsTest(unittest.TestCase):
-    """
-    Tests parsing an SBML file with only explicit equations (``assignmentRule``
-    elements).
-
-    This tests uses a modified model from the BioModels database.
-    https://www.ebi.ac.uk/biomodels/BIOMD0000000020.
-    """
-    @classmethod
-    def setUpClass(cls):
-        cls.model = myokit.formats.sbml.SBMLParser().parse_file(os.path.join(
-            DIR_FORMATS, 'sbml', 'HodgkinHuxley.xml'))
-
-    def test_parameters(self):
-        # Test all parameters show up in the Myokit model
-
-        # expected
-        parameters = [
-            'V',
-            'V_neg',
-            'E',
-            'I',
-            'i_Na',
-            'i_K',
-            'i_L',
-            'm',
-            'h',
-            'n',
-            'E_R',
-            'Cm',
-            'g_Na',
-            'g_K',
-            'g_L',
-            'E_Na',
-            'E_K',
-            'E_L',
-            'V_Na',
-            'V_K',
-            'V_L',
-            'alpha_m',
-            'beta_m',
-            'alpha_h',
-            'beta_h',
-            'alpha_n',
-            'beta_n'
-        ]
-
-        # test whether parameters are in myokit model
-        for param in parameters:
-            self.assertTrue(self.model.has_variable('sbml.' + param))
-
-    def test_units(self):
-        # Test units were parsed OK
-
-        # expected
-        param_unit_dict = {
-            'V': myokit.units.V * 10 ** (-3),
-            'V_neg': myokit.units.V * 10 ** (-3),
-            'E': myokit.units.V * 10 ** (-3),
-            'I': None,
-            'i_Na': None,
-            'i_K': None,
-            'i_L': None,
-            'm': None,
-            'h': None,
-            'n': None,
-            'E_R': myokit.units.V * 10 ** (-3),
-            'Cm': None,
-            'g_Na': None,
-            'g_K': None,
-            'g_L': None,
-            'E_Na': myokit.units.V * 10 ** (-3),
-            'E_K': myokit.units.V * 10 ** (-3),
-            'E_L': myokit.units.V * 10 ** (-3),
-            'V_Na': myokit.units.V * 10 ** (-3),
-            'V_K': myokit.units.V * 10 ** (-3),
-            'V_L': myokit.units.V * 10 ** (-3),
-            'alpha_m': None,
-            'beta_m': None,
-            'alpha_h': None,
-            'beta_h': None,
-            'alpha_n': None,
-            'beta_n': None
-        }
-
-        # test whether parameters have correct units
-        for param in param_unit_dict:
-            unit = param_unit_dict[param]
-            self.assertEqual(unit, self.model.get('sbml.' + param).unit())
-
-    def test_initial_values(self):
-        # Test the initial values for state variables
-
-        # expected
-        param_value_dict = {
-            'I': 0,
-            'E_R': -75,
-            'Cm': 1,
-            'g_Na': 120,
-            'g_K': 36,
-            'g_L': 0.3,
-            'E_Na': -190,
-            'E_K': -63,
-            'E_L': -85.613
-        }
-
-        # test whether parameters have correct initial values
-        for param in param_value_dict:
-            value = param_value_dict[param]
-            self.assertEqual(value, self.model.get('sbml.' + param).value())
-
-    def test_intermediate_expressions(self):
-        # Test the expressions for the intermediary variables
-
-        # expected
-        param_expr_dict = {
-            'V_neg': '-sbml.V',
-            'E': 'sbml.V + sbml.E_R',
-            'i_Na': 'sbml.g_Na * sbml.m ^ 3 * sbml.h * (sbml.V - sbml.V_Na)',
-            'i_K': 'sbml.g_K * sbml.n ^ 4 * (sbml.V - sbml.V_K)',
-            'i_L': 'sbml.g_L * (sbml.V - sbml.V_L)',
-            'V_Na': 'sbml.E_Na - sbml.E_R',
-            'V_K': 'sbml.E_K - sbml.E_R',
-            'V_L': 'sbml.E_L - sbml.E_R',
-            'alpha_m':
-            '0.1 * (sbml.V + 25) / (exp((sbml.V + 25) / 10) - 1)',
-            'beta_m': '4 * exp(sbml.V / 18)',
-            'alpha_h': '0.07 * exp(sbml.V / 20)',
-            'beta_h': '1 / (exp((sbml.V + 30) / 10) + 1)',
-            'alpha_n': '0.01 * (sbml.V + 10) / (exp((sbml.V + 10) / 10) - 1)',
-            'beta_n': '0.125 * exp(sbml.V / 80)'
-        }
-
-        # test whether intermediate expressions are correct
-        for param in param_expr_dict:
-            expr = param_expr_dict[param]
-            self.assertEqual(
-                'sbml.' + param, str(self.model.get('sbml.' + param).lhs()))
-            self.assertEqual(expr, str(self.model.get('sbml.' + param).rhs()))
-
-    def test_state_expressions(self):
-        # Test the expressions for the states
-
-        # expected
-        param_expr_dict = {
-            'm': 'sbml.alpha_m * (1 - sbml.m) - sbml.beta_m * sbml.m',
-            'h': 'sbml.alpha_h * (1 - sbml.h) - sbml.beta_h * sbml.h',
-            'n': 'sbml.alpha_n * (1 - sbml.n) - sbml.beta_n * sbml.n'
-        }
-
-        # test whether state expressions are correct
-        for param in param_expr_dict:
-            expr = param_expr_dict[param]
-            self.assertEqual(
-                'dot(sbml.%s)' % param,
-                str(self.model.get('sbml.' + param).lhs()))
-            self.assertEqual(expr, str(self.model.get('sbml.' + param).rhs()))
-'''
 
 if __name__ == '__main__':
     unittest.main()
