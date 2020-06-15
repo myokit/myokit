e9f132fada2f634c3eace1634b3eb00b5d56fb55
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-06-15 12:00:13 +0100

    New minor myokit version to indicate some backward compatibility.

--- myokit/_myokit_version.py
+++ myokit/_myokit_version.py
@@ -14,7 +14,8 @@ __release__ = False
 #  - Changes to minor indicate new features, possible slight backwards
 #    incompatibility
 #  - Changes to revision indicate bugfixes, tiny new features
-__version_tuple__ = 1, 30, 7
+#  - There is no significance to odd/even numbers
+__version_tuple__ = 1, 31, 0
 
 # String version of the version number
 __version__ = '.'.join([str(x) for x in __version_tuple__])
