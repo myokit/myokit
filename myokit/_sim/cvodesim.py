7ae0fadc0411b589c043e3cb5607379bfc505820
martinjrobins <martinjrobins@gmail.com>
2023-02-14 14:18:36 +0000

    #898 handle dict a bit better

--- myokit/_sim/cvodesim.py
+++ myokit/_sim/cvodesim.py
@@ -518,20 +518,10 @@ class Simulation(myokit.CModule):
         """
         Allows you to manually set the default state.
         """
-        if isinstance(state, dict):
-            state_expr = {
-                k: (
-                    s if isinstance(s, myokit.Expression)
-                    else myokit.Number(s)
-                )
-                for k, s in state.items()
-            }
-        else:
-            state_expr = [
-                s if isinstance(s, myokit.Expression) else myokit.Number(s)
-                for s in state
-            ]
-        self._default_state = self._model.map_to_state(state_expr)
+        self._default_state = [
+            s if isinstance(s, myokit.Expression) else myokit.Number(s)
+            for s in self._model.map_to_state(state)
+        ]
 
     def set_max_step_size(self, dtmax=None):
         """
