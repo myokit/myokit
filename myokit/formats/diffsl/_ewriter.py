8a91908f07ac72e418763d7eb6094d6771ed6777
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-05-24 09:47:05 +0000

    #1058 add diffsl conditional tests

--- myokit/formats/diffsl/_ewriter.py
+++ myokit/formats/diffsl/_ewriter.py
@@ -19,85 +19,122 @@ class DiffSLExpressionWriter(CBasedExpressionWriter):
     This :class:`ExpressionWriter <myokit.formats.ExpressionWriter>` writes
     equations for variables in DiffSL syntax.
 
-    For details of the language, see 
+    For details of the language, see
     https://martinjrobins.github.io/diffsl/introduction.html
     """
+
     def __init__(self):
         super().__init__()
 
+    # Literals and identifiers
+
+    # def _ex_name(self, e):
+    # def _ex_number(self, e):
+
+    # Functions
     def _ex_abs(self, e):
-        return self._ex_function(e, 'abs')
-    
+        return self._ex_function(e, "abs")
+
     def _ex_acos(self, e):
-        warnings.warn('Unsupported function: acos()')
+        warnings.warn("Unsupported function: acos()")
         return super()._ex_acos(e)
-    
-    # def _ex_and(self, e):
 
     def _ex_asin(self, e):
-        warnings.warn('Unsupported function: asin()')
+        warnings.warn("Unsupported function: asin()")
         return super()._ex_asin(e)
-    
+
     def _ex_atan(self, e):
-        warnings.warn('Unsupported function: atan()')
+        warnings.warn("Unsupported function: atan()")
         return super()._ex_atan(e)
 
     def _ex_ceil(self, e):
-        warnings.warn('Unsupported function: ceil()')
+        warnings.warn("Unsupported function: ceil()")
         return super()._ex_ceil(e)
-    
-    #def _ex_cos(self, e):
-    #def _ex_derivative(self, e):
-    #def _ex_divide(self, e):
-    #def _ex_equal(self, e):
-    #def _ex_exp(self, e):
+
+    # def _ex_cos(self, e):
+    # def _ex_derivative(self, e):
+    # def _ex_divide(self, e):
+    # def _ex_exp(self, e):
 
     def _ex_floor(self, e):
-        warnings.warn('Unsupported function: floor()')
+        warnings.warn("Unsupported function: floor()")
         return super()._ex_floor(e)
 
-    #def _ex_if(self, e):
-    #def _ex_less(self, e):
-    #def _ex_less_equal(self, e):
-    #def _ex_log(self, e):
+    # def _ex_log(self, e):
 
-    # TODO: log10(x) = log(x) / log(10)
+    # TODO: log10(x) = (log(x) / log(10))
     def _ex_log10(self, e):
-        warnings.warn('Unsupported function: log10()')
+        warnings.warn("Unsupported function: log10()")
         return super()._ex_log10(e)
 
     def _ex_minus(self, e):
         if isinstance(e[0], myokit.Exp) and isinstance(e[1], myokit.Number):
             if e[1].eval() == 1:
-                return f'expm1({self.ex(e[0][0])})'
+                return f"expm1({self.ex(e[0][0])})"
         if isinstance(e[1], myokit.Exp) and isinstance(e[0], myokit.Number):
             if e[0].eval() == 1:
-                return f'-expm1({self.ex(e[1][0])})'
+                return f"-expm1({self.ex(e[1][0])})"
         return super()._ex_minus(e)
-    
-    #def _ex_more(self, e):
-    #def _ex_more_equal(self, e):
-    #def _ex_multiply(self, e):
-    #def _ex_name(self, e):
-    #def _ex_not(self, e):
-    #def _ex_not_equal(self, e):
-    #def _ex_number(self, e):
-    #def _ex_or(self, e):
 
-    # TODO: Approximate with heaviside()
-    def _ex_piecewise(self, e):
-        warnings.warn('Unsupported function: piecewise')
-        return super()._ex_piecewise(e)
-    
-    #def _ex_plus(self, e):
+    # def _ex_multiply(self, e):
+    # def _ex_plus(self, e):
 
     def _ex_power(self, e):
-        return self._ex_function(e, 'pow')
+        return self._ex_function(e, "pow")
 
-    #def _ex_prefix_minus(self, e):
-    #def _ex_prefix_plus(self, e):
-    #def _ex_quotient(self, e):
-    #def _ex_remainder(self, e):
-    #def _ex_sin(self, e):
-    #def _ex_sqrt(self, e):
-    #def _ex_tan(self, e):
+    # def _ex_prefix_minus(self, e):
+    # def _ex_prefix_plus(self, e):
+    # def _ex_quotient(self, e):
+    # def _ex_remainder(self, e):
+    # def _ex_sin(self, e):
+    # def _ex_sqrt(self, e):
+    # def _ex_tan(self, e):
+
+    # Boolean operators
+    def _ex_and(self, e):
+        warnings.warn("Unsupported boolean operator: and")
+        return super()._ex_and(e)
+
+    def _ex_equal(self, e):
+        warnings.warn("Unsupported boolean operator: equal")
+        return super()._ex_equal(e)
+
+    def _ex_less(self, e):
+        warnings.warn("Unsupported boolean operator: less")
+        return super()._ex_less(e)
+
+    def _ex_less_equal(self, e):
+        warnings.warn("Unsupported boolean operator: less_equal")
+        return super()._ex_less_equal(e)
+
+    def _ex_more(self, e):
+        warnings.warn("Unsupported boolean operator: more")
+        return super()._ex_more(e)
+
+    def _ex_more_equal(self, e):
+        warnings.warn("Unsupported boolean operator: more_equal")
+        return super()._ex_more_equal(e)
+
+    def _ex_not(self, e):
+        warnings.warn("Unsupported boolean operator: not")
+        return super()._ex_not(e)
+
+    def _ex_not_equal(self, e):
+        warnings.warn("Unsupported boolean operator: not_equal")
+        return super()._ex_not_equal(e)
+
+    def _ex_or(self, e):
+        warnings.warn("Unsupported boolean operator: or")
+        return super()._ex_or(e)
+
+    # Conditional expressions
+
+    # TODO: Translate into heaviside()
+    def _ex_if(self, e):
+        warnings.warn("Unsupported conditional: if")
+        return super()._ex_if(e)
+
+    # TODO: Translate into heaviside()
+    def _ex_piecewise(self, e):
+        warnings.warn("Unsupported conditional: piecewise")
+        return super()._ex_piecewise(e)
--- myokit/tests/test_formats_diffsl.py
+++ myokit/tests/test_formats_diffsl.py
@@ -14,52 +14,133 @@ import myokit.formats.diffsl
 import myokit.tests
 
 from myokit import (
-    Number, PrefixPlus, PrefixMinus, Plus, Minus,
-    Multiply, Divide, Quotient, Remainder, Power, Sqrt,
-    Exp, Log, Log10, Sin, Cos, Tan, ASin, ACos, ATan, Floor, Ceil, Abs,
-    Not, And, Or, Equal, NotEqual, More, Less, MoreEqual, LessEqual,
-    If, Piecewise,
+    Number,
+    PrefixPlus,
+    PrefixMinus,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Quotient,
+    Remainder,
+    Power,
+    Sqrt,
+    Exp,
+    Log,
+    Log10,
+    Sin,
+    Cos,
+    Tan,
+    ASin,
+    ACos,
+    ATan,
+    Floor,
+    Ceil,
+    Abs,
+    Not,
+    And,
+    Or,
+    Equal,
+    NotEqual,
+    More,
+    Less,
+    MoreEqual,
+    LessEqual,
+    If,
+    Piecewise,
 )
 
 from myokit.tests import TemporaryDirectory, WarningCollector, DIR_DATA
 
 
 class DiffSLExpressionWriterTest(myokit.tests.ExpressionWriterTestCase):
-    """ Test conversion to DiffSL syntax. """
-    _name = 'diffsl'
+    """Test conversion to DiffSL syntax."""
+
+    _name = "diffsl"
     _target = myokit.formats.diffsl.DiffSLExpressionWriter
 
     def test_functions(self):
         a, b = self.ab
 
-        self.eq(Abs(a), 'abs(a)')
-        self.eq(Cos(a), 'cos(a)')
-        self.eq(Exp(a), 'exp(a)')
-        self.eq(Log(a), 'log(a)')
-        self.eq(Sin(a), 'sin(a)')
-        self.eq(Sqrt(a), 'sqrt(a)')
-        self.eq(Power(a, b), 'pow(a, b)')
-        self.eq(Tan(a), 'tan(a)')
+        self.eq(Abs(a), "abs(a)")
+        self.eq(Cos(a), "cos(a)")
+        self.eq(Exp(a), "exp(a)")
+        self.eq(Log(a), "log(a)")
+        self.eq(Sin(a), "sin(a)")
+        self.eq(Sqrt(a), "sqrt(a)")
+        self.eq(Power(a, b), "pow(a, b)")
+        self.eq(Tan(a), "tan(a)")
 
-        with WarningCollector() as c:
-            self.eq(ACos(a), 'acos(a)')
-        self.assertIn('Unsupported', c.text())
+        with WarningCollector() as wc:
+            self.eq(ACos(a), "acos(a)")
+        self.assertIn("Unsupported", wc.text())
 
-        with WarningCollector() as c:
-            self.eq(ASin(a), 'asin(a)')
-        self.assertIn('Unsupported', c.text())
+        with WarningCollector() as wc:
+            self.eq(ASin(a), "asin(a)")
+        self.assertIn("Unsupported", wc.text())
 
-        with WarningCollector() as c:
-            self.eq(ATan(a), 'atan(a)')
-        self.assertIn('Unsupported', c.text())
+        with WarningCollector() as wc:
+            self.eq(ATan(a), "atan(a)")
+        self.assertIn("Unsupported", wc.text())
 
-        with WarningCollector() as c:
-            self.eq(Ceil(a), 'ceil(a)')
-        self.assertIn('Unsupported', c.text())
+        with WarningCollector() as wc:
+            self.eq(Ceil(a), "ceil(a)")
+        self.assertIn("Unsupported", wc.text())
 
-        with WarningCollector() as c:
-            self.eq(Floor(a), 'floor(a)')
-        self.assertIn('Unsupported', c.text())
+        with WarningCollector() as wc:
+            self.eq(Floor(a), "floor(a)")
+        self.assertIn("Unsupported", wc.text())
 
-if __name__ == '__main__':
+    def test_boolean_operators(self):
+        a, b = self.ab
+
+        with WarningCollector() as wc:
+            self.eq(And(a, b), "(a && b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(Equal(a, b), "(a == b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(Less(a, b), "(a < b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(LessEqual(a, b), "(a <= b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(More(a, b), "(a > b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(MoreEqual(a, b), "(a >= b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(Not(a), "(!(a))")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(NotEqual(a, b), "(a != b)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(Or(a, b), "(a || b)")
+        self.assertIn("Unsupported", wc.text())
+
+    def test_conditional_expressions(self):
+        a, b, c, d = self.abcd
+
+        with WarningCollector() as wc:
+            self.eq(If(Equal(a, b), c, d), "((a == b) ? c : d)")
+        self.assertIn("Unsupported", wc.text())
+
+        with WarningCollector() as wc:
+            self.eq(Piecewise(Equal(a, b), c, d), "((a == b) ? c : d)")
+        self.assertIn("Unsupported", wc.text())
+
+
+if __name__ == "__main__":
     unittest.main()
