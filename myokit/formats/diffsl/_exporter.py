7afef9e36338c6ee6abc5b3ddc2cd829224d6166
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-08-26 16:24:53 +0000

    #1058 Commit functional diffsl writer

--- myokit/formats/diffsl/_exporter.py
+++ myokit/formats/diffsl/_exporter.py
@@ -11,8 +11,6 @@ import warnings
 import myokit
 import myokit.formats
 import myokit.lib.guess as guess
-import myokit.lib.hh as hh
-import myokit.lib.markov as markov
 
 import myokit.formats.diffsl as diffsl
 
@@ -48,15 +46,16 @@ class DiffSLExporter(myokit.formats.Exporter):
             Not implemented!
 
         """
+        # Test if can write in dir (raises exception if can't)
+        self._test_writable_dir(os.path.dirname(path))
+
         # Check model validity
         try:
             model.validate()
         except myokit.MyokitError:
             raise myokit.ExportError('DiffSL export requires a valid model.')
 
-        # Rewrite model so that any Markov models have a 1-sum(...) state
-        # This also clones the model, so that changes can be made
-        model = markov.convert_markov_models_to_compact_form(model)
+        model = model.clone()
 
         # Remove all model bindings apart from time and pace
         _ = myokit._prepare_bindings(model, {
@@ -64,37 +63,54 @@ class DiffSLExporter(myokit.formats.Exporter):
             'pace': 'pace',
         })
 
+        # Prepare to replace all state derivatives on the rhs with intermediary
+        # variables. First, add a temporary rhs reference for all state
+        # derivatives that do not already have an rhs reference or an
+        # intermediary variable.
+        tmp_vars = []
+        for state in model.states():
+            if isinstance(state.rhs(), myokit.Name):
+                continue  # Derivative already has an intermediary variable
+
+            if list(state.refs_by()):
+                continue  # Derivative already has at least one rhs reference
+
+            # Add a temporary rhs reference
+            var = state.parent().add_variable_allow_renaming(
+                'tmp_DiffSL_' + state.name() + '_Myokit'
+            )
+            var.set_rhs(state.lhs())
+            tmp_vars.append(var)
+
         # Replace state derivatives on rhs with intermediary variables
         # e.g. set `dxdt` = rhs of `dot(x)`; then where `dot(x)` is found in
         # all other rhs's, replace it with `dxdt`; also, set `dot(x) = dxdt`.
         model.remove_derivative_references()
 
+        # Reset temporary rhs references
+        for var in tmp_vars:
+            var.set_rhs(0)
+
         # Remove hardcoded stimulus protocol, if any
         guess.remove_embedded_protocol(model)
 
-        # Get / try to guess model variables
+        # Get / try to guess some model variables
         time = model.time()  # engine.time
         pace = model.binding('pace')  # engine.pace
+        i_diff = model.binding('diffusion_current')
         v_m = guess.membrane_potential(model)  # Vm
         c_m = guess.membrane_capacitance(model)  # Cm
-        i_diff = model.binding('diffusion_current')  # Idiff
-        i_ion = model.label('cellular_current')  # Iion
-        membrane_currents = guess.membrane_currents(model)  # [Iion,...]
 
-        # Variables to remove or exclude from output
-        vars_to_remove = {pace, i_diff, i_ion}
-        vars_to_exclude = {v_m, time}
-
-        # Remove potentials from membrane currents, if present
-        i = 0
-        while i < len(membrane_currents):
-            if membrane_currents[i].unit() in [
+        # Guess membrane currents, excluding potentials if present
+        membrane_currents = {
+            x
+            for x in guess.membrane_currents(model)
+            if x.unit()
+            not in [
                 myokit.parse_unit('mV'),
                 myokit.parse_unit('V'),
-            ]:
-                membrane_currents.pop(i)
-            else:
-                i += 1
+            ]
+        }  # {Iion,...}
 
         # Ensure Vm is a state, so that expressions
         # depending on it are not seen as constants.
@@ -146,12 +162,13 @@ class DiffSLExporter(myokit.formats.Exporter):
                     )
                 except myokit.IncompatibleUnitError:
                     warnings.warn(
-                        "Unable to convert time units "
+                        'Unable to convert time units '
                         + str(time_unit)
-                        + " to recommended units of ms."
+                        + ' to recommended units of ms.'
                     )
 
-        # Remove unwanted variables e.g. pacing, diffusion current, i_ion
+        # Remove unwanted variables e.g. pacing, diffusion current
+        vars_to_remove = {pace, i_diff}
         vars_to_remove.discard(None)
         for var in vars_to_remove:
             # Replace references to these variables with 0
@@ -164,15 +181,16 @@ class DiffSLExporter(myokit.formats.Exporter):
             var.parent().remove_variable(var)
 
             # Remove from currents, if present
-            if var in membrane_currents:
-                membrane_currents.remove(var)
+            membrane_currents.discard(var)
 
-        # Prepare to remove unused variables
-        # Set Vm to the sum of currents so that all currents count as used
+        # Prepare to remove unused vars - sum currents so they're all 'used'
+        curr_sum_var = v_m.parent().add_variable_allow_renaming(
+            'tmp_DiffSL_SuM_oF_cUrReNtS_Myokit'
+        )
         rhs = myokit.Number(0)
-        for v in membrane_currents:
+        for v in sorted(membrane_currents, key=lambda x: x.qname()):
             rhs = myokit.Plus(rhs, v.lhs())
-        v_m.set_rhs(rhs)
+        curr_sum_var.set_rhs(rhs)
 
         # Remove all labels so that they register as unused
         for _, var in model.labels():
@@ -181,67 +199,10 @@ class DiffSLExporter(myokit.formats.Exporter):
         # Remove all unused variables
         model.validate(remove_unused_variables=True)
 
-        # Find special Hodgkin-Huxley variables
-        hh_states = set()
-        alphas = dict()
-        betas = dict()
-        taus = dict()
-        infs = dict()
-
-        # If an inf/tau/alpha/beta is used by more than one state, we create a
-        # copy for each user, so that we can give the variables the appropriate
-        # name (e.g. x_inf, y_inf, z_inf) etc.
-        def renamable(var):
-            srefs = [v for v in var.refs_by() if v.is_state()]
-            if len(srefs) > 1:
-                v = var.parent().add_variable_allow_renaming(var.name())
-                v.set_rhs(myokit.Name(var))
-                v.set_unit(var.unit())
-                return v
-            return var
-
-        # Find HH state variables and their infs, taus, alphas, betas
-        for var in model.states():
-            inf_tau = hh.get_inf_and_tau(var, v_m)
-            if inf_tau is not None:
-                vars_to_exclude.add(var)
-                hh_states.add(var)
-                infs[renamable(inf_tau[0])] = var
-                taus[renamable(inf_tau[1])] = var
-                continue
-
-            alpha_beta = hh.get_alpha_and_beta(var, v_m)
-            if alpha_beta is not None:
-                vars_to_exclude.add(var)
-                hh_states.add(var)
-                alphas[renamable(alpha_beta[0])] = var
-                betas[renamable(alpha_beta[1])] = var
-                continue
-
-        hh_variables = (set(alphas.keys())
-                        | set(betas.keys())
-                        | set(taus.keys())
-                        | set(infs.keys()))
-
-        # Find Markov models (must be run before time conversion)
-        markov_models = markov.find_markov_models(model)
-
-        # Add time conversion factor for states and HH variables
-        if time_factor is not None:
-            for var in model.states():
-                var.set_rhs(myokit.Multiply(var.rhs(), time_factor_inv))
-            for var in alphas:
-                var.set_rhs(myokit.Multiply(var.rhs(), time_factor_inv))
-            for var in betas:
-                var.set_rhs(myokit.Multiply(var.rhs(), time_factor_inv))
-            for var in taus:
-                var.set_rhs(myokit.Multiply(var.rhs(), time_factor))
-
         # Create variable names
         # The following strategy is followed:
-        #  - HH variables are named after their state
-        #  - All other variables are checked for special names, and changed if
-        #    necessary
+        #  - Variables are named after their state
+        #  - Variables are checked for special names, and changed if necessary
         #  - Any conflicts are resolved in a final step
 
         sep = 'Z'
@@ -268,11 +229,6 @@ class DiffSLExporter(myokit.formats.Exporter):
         # Create initial variable names
         var_to_name = collections.OrderedDict()
         for var in model.variables(deep=True, sort=True):
-
-            # Delay naming of HH variables until their state has a name
-            if var in hh_variables:
-                continue
-
             # Start from simple variable name
             name = var.name()
 
@@ -299,13 +255,9 @@ class DiffSLExporter(myokit.formats.Exporter):
 
         # Check for conflicts with known keywords
         from . import keywords
-        reserved = [
-            'Iion',
-        ]
+
         needs_renaming = collections.OrderedDict()
         for keyword in keywords:
-            needs_renaming[keyword] = []
-        for keyword in reserved:
             needs_renaming[keyword] = []
 
         # Find naming conflicts, create inverse mapping
@@ -342,16 +294,6 @@ class DiffSLExporter(myokit.formats.Exporter):
         # Remove reverse mappings
         del name_to_var, needs_renaming
 
-        # Create names for HH variables
-        for var, state in alphas.items():
-            var_to_name[var] = var_to_name[state] + sep + 'alpha'
-        for var, state in betas.items():
-            var_to_name[var] = var_to_name[state] + sep + 'beta'
-        for var, state in taus.items():
-            var_to_name[var] = var_to_name[state] + sep + 'tau'
-        for var, state in infs.items():
-            var_to_name[var] = var_to_name[state] + sep + 'inf'
-
         # Create naming function
         def lhs(e):
             if isinstance(e, myokit.Derivative):
@@ -367,103 +309,120 @@ class DiffSLExporter(myokit.formats.Exporter):
         e = diffsl.DiffSLExpressionWriter()
         e.set_lhs_function(lhs)
 
-        # Test if can write in dir (raises exception if can't)
-        self._test_writable_dir(os.path.dirname(path))
+        # Sort equations in solvable order (grouped by component)
+        sorted_eqs = model.solvable_order()
 
-        # Create diffsl string to write to file
-        export_str = ''
+        # Variables to be excluded from output or handled separately
+        handled_vars = (
+            {time}
+            | set([v.rhs().var() for v in model.states()])
+            | set([v.lhs().var() for v in model.states()])
+        )
+
+        # Lines to export to file
+        file_lines = []
+        tab = '  '
 
         # Add metadata
-        export_str += '/*\n'
-        export_str += 'This file was generated by Myokit.\n'
+        file_lines.append('/*')
+        file_lines.append('This file was generated by Myokit.')
         if model.meta:
-            export_str += '\n'
+            file_lines.append('')
             for key, val in sorted(model.meta.items()):
-                export_str += key + ': ' + val + '\n'
-            export_str += '\n'
-        export_str += '*/\n\n'
+                file_lines.append(key + ': ' + val)
+            file_lines.append('')
+        file_lines.append('*/')
+        file_lines.append('')
 
-        # Add empty parameter list
-        export_str += f'in = [ ]\n\n'
+        # Add empty input parameter list
+        file_lines.append('/* Input parameters */')
+        file_lines.append('in = [ ]')
+        file_lines.append('')
 
         # Add constants
-        constants = list(model.variables(const=True, sort=True))
-        if constants:
-            export_str += '/* Constants */\n'
-            for v in constants:
-                export_str += lhs(v) + ' { ' + e.ex(v.eq().rhs) + ' }\n'
-            export_str += '\n'
+        constants = set(model.variables(deep=True, const=True)) - handled_vars
+        for label, eq_list in sorted_eqs.items():
+            const_eqs = [
+                eq for eq in eq_list.equations() if eq.lhs.var() in constants
+            ]
+            if const_eqs:
+                file_lines.append(f'/* Constants: {label} */')
+                for eq in const_eqs:
+                    v = eq.lhs.var()
+                    rhs = e.ex(eq.rhs)
+                    unit = '' if v.unit() is None else f' {v.unit()}'
+                    file_lines.append(
+                        lhs(v) + ' { ' + rhs + ' } ' +
+                        f'/* {v.name()}{unit} */'
+                    )
+                file_lines.append('')
 
-        # Add initial conditions
-        export_str += 'u_i {\n'
+        # Add initial conditions `u_i`
+        file_lines.append('/* Initial conditions */')
+        file_lines.append('u_i {')
         for v in model.states():
             initial_value = myokit.float.str(v.initial_value(True))
-            export_str += f'  {lhs(v)} = {initial_value},\n'
-        export_str += '}\n\n'
+            unit = '' if v.unit() is None else f' {v.unit()}'
+            file_lines.append(
+                f'{tab}{lhs(v)} = {initial_value}, /* {v.name()}{unit} */'
+            )
+        file_lines.append('}')
+        file_lines.append('')
+
+        # Add initial conditions `dudt_i`
+        file_lines.append('dudt_i {')
+        for v in model.states():
+            initial_value = myokit.float.str(v.initial_value(True))
+            file_lines.append(
+                f'{tab}{lhs(v.rhs())} = {initial_value},'
+            )
+        file_lines.append('}')
+        file_lines.append('')
 
         # Add remaining variables
-        # TODO: sort variables in order of dependence
-        for c in model.components(sort=True):
-            todo = c.variables(deep=True, sort=True)
-            todo = [v for v in todo if v not in vars_to_exclude]
-            if todo:
-                export_str += '/* ' + c.name() + ' */\n'
-                for v in todo:
-                    if isinstance(v, myokit.Derivative):
-                        export_str += 'DERIVATIVE'
-                    export_str += lhs(v) + ' { ' + e.ex(v.eq().rhs) + ' } '
-                    export_str += '/* ' + v.name() + ' */\n'
-                export_str += '\n'
+        todo = set(model.variables(deep=True, const=False)) - handled_vars
+        for label, eq_list in sorted_eqs.items():
+            todo_eqs = [
+                eq for eq in eq_list.equations() if eq.lhs.var() in todo
+            ]
+            if todo_eqs:
+                file_lines.append(f'/* Variables: {label} */')
+                for eq in todo_eqs:
+                    v = eq.lhs.var()
+                    rhs = e.ex(eq.rhs)
+                    unit = '' if v.unit() is None else f' {v.unit()}'
+                    file_lines.append(
+                        lhs(v) + ' { ' + rhs + ' } ' +
+                        f'/* {v.name()}{unit} */'
+                    )
+                file_lines.append('')
 
-        # Add initial conditions
-        export_str += 'dudt_i {\n'
+        # Solve
+        file_lines.append('F_i {')
         for v in model.states():
-            initial_value = myokit.float.str(v.initial_value(True))
-            export_str += f'  {lhs(v)} = {initial_value},\n'
-        export_str += '}\n\n'
+            file_lines.append(f'{tab}{lhs(v.rhs())},')
+        file_lines.append('}')
+        file_lines.append('')
 
-        # Add sum of currents variable
-        export_str += '/* Sum of currents */\n'
-        export_str += 'Iion { ' + \
-            ' + '.join([lhs(v) for v in membrane_currents]) + ' }\n\n'
-
-        # Add solution methods for Markov models
-        markov_states = set()
-        for states in markov_models:
-            export_str += '/* Markov model */\n'
-            export_str += '{\n'
-            for state in states:
-                if state.is_state():
-                    export_str += '  ' + lhs(state) + '\n'
-                    markov_states.add(state)
-            export_str += '}\n\n'
-
-        # Solve all non-Markovian and non-HH variables with CVODE
-        cvode_states = [
-            x for x in model.states() if not (
-                x == v_m or x in markov_states or x in hh_states)]
-        if cvode_states:
-            export_str += '/* Solve non-HH and non-Markov states with CVODE */\n'
-            export_str += 'F_i {\n'
-            for v in cvode_states:
-                export_str += '  ' + lhs(v) + '\n'
-            export_str += '}\n\n'
-
-            export_str += 'G_i {\n'
-            for v in cvode_states:
-                export_str += '  ' + lhs(v) + ' { ' + e.ex(v.eq().rhs) + ' }\n'
-            export_str += '}\n\n'
+        file_lines.append('G_i {')
+        for v in model.states():
+            file_lines.append(f'{tab}{e.ex(v.eq().rhs.rhs())},')
+        file_lines.append('}')
+        file_lines.append('')
 
         # Output all currents and state variables
-        export_str += '/* Output all currents and state variables */\n'
-        export_str += 'out_i {\n'
-        for vlhs in sorted(lhs(v) for v in set(membrane_currents).union(model.states())):
-            export_str += f'  {vlhs}\n'
-        export_str += '}\n'
+        file_lines.append('/* Output all currents and state variables */')
+        file_lines.append('out_i {')
+        out_vars = sorted(membrane_currents.union(model.states()), key=lambda x: x.qname())
+        for v in out_vars:
+            file_lines.append(f'{tab}{lhs(v)},')
+        file_lines.append('}')
+        file_lines.append('')
 
         # Write model to file
+        file_str = '\n'.join(file_lines)
         with open(path, 'w') as f:
-            f.write(export_str)
+            f.write(file_str)
 
     def supports_model(self):
         """ See :meth:`myokit.formats.Exporter.supports_model()`. """
