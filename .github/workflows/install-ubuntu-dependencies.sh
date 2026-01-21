abda1046ca02a81d7a59335557762429ca0492c2
Michael Clerx <michael.clerx@nottingham.ac.uk>
2026-01-21 11:51:55 +0000

    Setting up test for cvode 6 and earlier warning suppression

--- .github/workflows/install-ubuntu-dependencies.sh
+++ .github/workflows/install-ubuntu-dependencies.sh
@@ -9,13 +9,6 @@ set -ev
 wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null;
 echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list;
 
-# Update apt and install "oneapi" opencl driver
-apt-get -qq update;
-apt-get install -y intel-oneapi-runtime-libs intel-oneapi-runtime-opencl;
-
-# Install OpenCL headers
-apt-get install -y opencl-headers ocl-icd-opencl-dev;
-
 # Install sundials
 apt-get install -y libsundials-dev;
 
